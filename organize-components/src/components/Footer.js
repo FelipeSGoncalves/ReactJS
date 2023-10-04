b18d6123a553e9c898e5d391379078e3ddc22e2b
FelipeSGoncalves <felipe.goncalves.foz@Hotmail.com>
2023-10-04 20:18:19 -0300

    aprendendo a utilizar Vite

--- organize-components/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Footer() {
-    return (
-        <footer>
-            <small>© 2023 Gonçalves. Todos os direitos reservados."</small>
-        </footer>
-    )
-}
